a077c1341d24ecf5ef87de848ff26d34fc56a2e4
Almantas <internet622@gmail.com>
2023-06-07 13:42:06 +0300

    Update login.js

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -11,7 +11,7 @@ const Login = () => {
 
   const handleClick = async () => {
     try {
-      await axios.post('http://localhost:3000/api/login', { username, password });
+      await axios.post('https://react-next-redux-mongodb-pizza.vercel.app/api/login', { username, password });
       router.push('/admin');
     } catch (err) {
       console.log(err);
